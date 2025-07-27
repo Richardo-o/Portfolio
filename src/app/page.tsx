46a2017d0d5d34c00bbc19ad66cb1424f2b22fcb
Richardo-o <rickysilva04205@gmail.com>
2025-07-27 17:06:15 -0300

    Adicionando tecnologias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,122 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="  py-10">
+      <main className="w-full">
+        {/* Seção sobre mim */}
+        <div
+          id="my-person"
+          className="flex flex-col lg:flex-row items-center justify-center gap-10 p-30"
+        >
+          {/* Texto */}
+          <div className="max-w-xl text-center lg:text-left">
+            <p className="text-lg font-medium mb-2">I am Developer!</p>
+            <h1 className="text-4xl font-bold text-white-900 mb-4">
+              Ricardo Estevam
+            </h1>
+            <p className="white-700 leading-relaxed">
+              Atuo como desenvolvedor full stack. Possuo conhecimentos tanto em
+              tecnologias de frontend quanto de backend, o que me permite
+              desenvolver aplicações web completas, modernas e eficientes. Estou
+              em constante aprimoramento técnico, com foco na criação de
+              soluções escaláveis, seguras e alinhadas às boas práticas de
+              desenvolvimento. Tenho comprometimento com a qualidade do código,
+              usabilidade e desempenho das aplicações que construo, além de
+              prezar pelo trabalho em equipe e pela evolução contínua na área da
+              tecnologia.
+            </p>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          {/* Imagem e botão */}
+          <div className="flex flex-col items-center mt-10 lg:mt-0">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={300}
+              height={300}
+              alt="my person"
+              src="/eu.png"
+              className="w-48 h-48 sm:w-60 sm:h-60 lg:w-72 lg:h-72 rounded-full object-cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              style={{ backgroundColor: "#6057FD" }}
+              className="text-white px-6 py-2 rounded mt-4 shadow-md hover:brightness-110 transition"
+            >
+              Meu Currículo
+            </button>
+          </div>
         </div>
+
+        {/* Seção de Tecnologias */}
+        <div
+          id="container-mid"
+          className="w-full p-30 mt-20 flex flex-col items-center"
+        >
+          <h1 className="text-3xl font-bold text-center">Tecnologias</h1>
+
+          <div className="flex justify-around w-full mt-10 gap-10">
+            <div id="tecs" className="flex flex-col gap-10">
+              <Image
+                width={80}
+                height={80}
+                alt="React"
+                src="/react.png"
+                className="w-20 h-20 sm:w-24 sm:h-24 m-auto"
+              />
+              <p className="text-center text-lg">
+                Biblioteca JavaScript focada na construção de interfaces de
+                usuário reativas e eficientes. Ideal para criar componentes
+                reutilizáveis e páginas dinâmicas com alta performance.
+              </p>
+            </div>
+
+            <div id="tecs" className="flex flex-col gap-10 " >
+              <Image
+                width={80}
+                height={80}
+                alt="Next.js"
+                src="/next.png"
+                className="w-20 h-20 sm:w-24 sm:h-24 m-auto"
+              />
+              <p className="text-center text-lg">
+                Framework para React que permite renderização do lado do servidor
+                (SSR), geração de sites estáticos (SSG) e otimizações avançadas de
+                performance. Excelente para SEO e aplicações modernas.
+              </p>
+              </div>
+              <div id="tecs" className="flex flex-col gap-10 " >
+                <Image
+                  width={80}
+                  height={80}
+                  alt="TypeScript"
+                  src="/ts.png"
+                  className="w-20 h-20 sm:w-24 sm:h-24 m-auto"
+                />
+                <p className="text-center text-lg">
+                  Superset do JavaScript que adiciona tipagem estática ao código,
+                  ajudando a prevenir erros em tempo de desenvolvimento e
+                  proporcionando maior segurança e escalabilidade ao projeto.
+                </p>
+              </div>
+            
+            <div id="tecs" className="flex flex-col gap-10" >
+              <Image
+                width={80}
+                height={80}
+                alt="MySQL"
+                src="/mysql.png"
+                className="w-20 h-20 sm:w-24 sm:h-24 rounded-full m-auto"
+              />
+              <p className="text-center text-lg">
+                Sistema de gerenciamento de banco de dados relacional. Utilizado
+                para armazenar, organizar e consultar dados estruturados de forma
+                eficiente, sendo amplamente adotado no mercado.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Seção futura */}
+        <div id="section-2" className="w-full h-52 mt-20"></div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
